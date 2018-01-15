31772f23da5e4975af9d5a229dd909ded04ebeca
randhir <randhiricadbg@gmail.com>
2018-01-15 18:16:14 +0530

    before foreach

--- src/main/scala/edu/knoldus/Application.scala
+++ src/main/scala/edu/knoldus/Application.scala
@@ -5,7 +5,7 @@ import org.apache.log4j.Logger
 object Application extends App {
   val log = Logger.getLogger(this.getClass)
   val operation = new Operation
-  val number = 97
-  val series = operation.innerCall(List(), number)
+  val number = 64
+  val series = operation.innerCall(List(1,4,8,9,12,16), number)
   log.info(series)
 }
--- src/main/scala/edu/knoldus/Operation.scala
+++ src/main/scala/edu/knoldus/Operation.scala
@@ -2,10 +2,20 @@ package edu.knoldus
 
 class Operation {
   def innerCall(list: List[Int], result: Int): List[Int] = {
+
+    def contain(list: List[Int], res: Int): Boolean = {
+      list match {
+        case Nil => false
+        case head :: tail if head == res => true
+        case head :: tail => contain(tail, res)
+      }
+    }
+
     list match {
       case _ if result == 1 => list
-      case _ if result % 2 == 0 => innerCall((result / 2) :: list, result / 2)
-      case _ if result % 2 == 1 => innerCall(((3 * result + 1) / 2) :: list, (3 * result + 1) / 2)
+      case _ if contain(list, result / 2) == false && result % 2 == 0 => innerCall((result / 2) :: list, result / 2)
+      case _ if contain(list, (3 * result + 1) / 2) == false && result % 2 == 1 => innerCall(((3 * result + 1) / 2) :: list, (3 * result + 1) / 2)
+      case _ => list
     }
   }
 }
