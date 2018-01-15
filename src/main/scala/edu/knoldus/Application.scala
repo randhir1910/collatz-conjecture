aad316ae84b143bcb02479e8438595b716410a29
randhir <randhiricadbg@gmail.com>
2018-01-15 21:52:52 +0530

    scalastyle successful

--- src/main/scala/edu/knoldus/Application.scala
+++ src/main/scala/edu/knoldus/Application.scala
@@ -6,7 +6,7 @@ object Application extends App {
   val log = Logger.getLogger(this.getClass)
   val operation = new Operation
   var list: List[Int] = List.empty
-  for (x <- 2 to 10) {
+  for (x <- 2 to 15) {
     val tempList: List[Int] = operation.innerCall(list, x)
     list = tempList
   }
--- src/main/scala/edu/knoldus/Operation.scala
+++ src/main/scala/edu/knoldus/Operation.scala
@@ -13,8 +13,8 @@ class Operation {
 
     list match {
       case _ if result == 1 => list
-      case _ if contain(list, result / 2) == false && result % 2 == 0 => innerCall((result / 2) :: list, result / 2)
-      case _ if contain(list, (3 * result + 1)) == false && result % 2 == 1 => innerCall(((3 * result + 1)) :: list, (3 * result + 1))
+      case _ if result % 2 == 0 && contain(list, result / 2) == false => innerCall((result / 2) :: list, result / 2)
+      case _ if result % 2 == 1 && contain(list, (3 * result + 1)) == false => innerCall(((3 * result + 1)) :: list, (3 * result + 1))
       case _ => list
     }
   }
